389cc79d7edd8827021b4f7f911eef29f1c9da7e
Gabriel Pavan <gabrielpavan2014@outlook.com>
2025-11-01 14:01:25 -0300

    Init

---
+++ src/main/java/com/unesc/unireddit/UniredditApplication.java
@@ -0,0 +1,13 @@
+package com.unesc.unireddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniredditApplication.class, args);
+	}
+
+}
